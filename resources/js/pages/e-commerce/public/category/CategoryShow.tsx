6ca6079da14f20b4341ef05132eaf0cfb5a87503
Dinesh Wijethunga <dineshlw8@gmail.com>
2025-11-15 00:29:14 +0400

    category product

--- resources/js/pages/e-commerce/public/category/CategoryShow.tsx
+++ resources/js/pages/e-commerce/public/category/CategoryShow.tsx
@@ -284,7 +284,7 @@ export default function CategoryShow() {
                         {products && products.length > 0 ? (
                             <>
                                 <div
-                                    className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-8">
+                                    className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-8">
                                     {products.map((product) => (
                                         <ProductCard key={product.id} product={product} />
                                     ))}
